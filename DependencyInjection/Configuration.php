22ddb982c7cdbeaf1652937b43fe93274406c96c
Cedric Ziel <cedric.ziel@instana.com>
2021-01-29 10:44:01 +0100

    Pass root node name to TreeBuilder

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,7 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
+        $treeBuilder = new TreeBuilder('chaplean_kanbanize_api_client');
         $treeBuilder->root('chaplean_kanbanize_api_client');
 
         return $treeBuilder;
